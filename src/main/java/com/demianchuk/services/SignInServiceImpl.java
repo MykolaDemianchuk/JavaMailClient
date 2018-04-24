52fa116b6796df05f04d8b3a6a266b6b27b3c8a7
Mykola Demianchuk <nicksetone@gmail.com>
2018-04-24 14:57:11 -0500

    Add logger to log MessagingException

--- src/main/java/com/demianchuk/services/SignInServiceImpl.java
+++ src/main/java/com/demianchuk/services/SignInServiceImpl.java
@@ -1,9 +1,13 @@
 package com.demianchuk.services;
 
 import com.demianchuk.model.MailClient;
+import org.apache.log4j.Logger;
+
+import javax.mail.AuthenticationFailedException;
 import javax.mail.MessagingException;
 
 public class SignInServiceImpl implements SignInService {
+    private static final Logger LOGGER = Logger.getLogger(SendEmailServiceImpl.class);
 
     @Override
     public void signIn(MailClient client, String username, char[] password) throws Exception{
@@ -11,8 +15,10 @@ public class SignInServiceImpl implements SignInService {
         client.createSession();
         try {
             client.getTransport().connect();
-        } catch (MessagingException e) {
+        } catch (AuthenticationFailedException e) {
             throw new Exception("Invalid credentials!");
+        } catch (MessagingException e) {
+            LOGGER.warn(e.getMessage(), e);
         }
     }
 }
